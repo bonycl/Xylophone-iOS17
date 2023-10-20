51cf3ff2c423eaa6dc96a4d94d985fa07b16e076
Dinar Shakirov <139841080+bonycl@users.noreply.github.com>
2023-10-20 20:40:09 +0700

    setup ui

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,59 @@
 //
 
 import UIKit
+import AVFoundation
+
+
+
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+            
+        //print (sender.currentTitle!)
+        //sender.alpha = 0.5
+        
+        playSound(soundName: sender.currentTitle!)
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(Int(0.2)), execute: {
+            sender.alpha = 0.5
+            
+            
+        })
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(Int(1)), execute: {
+            
+            sender.alpha = 1
+            print(sender.alpha)
+        })
+    }
+    
+    func playSound(soundName: String) {
+        guard let path = Bundle.main.path(forResource: soundName, ofType:"wav") else {
+            return }
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    //func chngOpacity (viewName: UIImageView){
+        
+   // }
+    
+    
+    
+    
 }
+
 
